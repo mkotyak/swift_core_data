1ba3ac0d9124f1af6b9ea23950e8cdc33e259c61
mkotyak <kotyak.maria@gmail.com>
2023-11-30 10:47:24 +0300

    Initialized TestCoreDataStack

---
+++ Chapter7/CampgroundManagerTests/TestCoreDataStack.swift
@@ -0,0 +1,23 @@
+import CampgroundManager
+import CoreData
+import Foundation
+
+class TestCoreDataStack: CoreDataStack {
+  override init() {
+    super.init()
+    
+    let container = NSPersistentContainer(
+      name: CoreDataStack.modelName,
+      managedObjectModel: CoreDataStack.model
+    )
+    
+    container.persistentStoreDescriptions[0].url = URL(fileURLWithPath: "/dev/null")
+    container.loadPersistentStores { _, error in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    }
+    
+    self.storeContainer = container
+  }
+}
